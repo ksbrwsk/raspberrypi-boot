1e85e9bae83cc3da1d2bd7239c1f8c6c298e1e23
Kai Saborowski <kai.saborowski@googlemail.com>
2015-03-06 12:20:26 +0100

    removed unused methods

--- raspberrypi-boot-server/src/main/resources/static/js/endless.js
+++ raspberrypi-boot-server/src/main/resources/static/js/endless.js
@@ -70,13 +70,7 @@ $(function	()	{
 			
 		return false;	
 	});
-	
-	//Logout Confirmation
-	$('#logoutConfirm').popup({
-		pagecontainer: '.container',
-		 transition: 'all 0.3s'
-	});
-	
+
 	//scroll to top of the page
 	$("#scroll-to-top").click(function()	{
 		$("html, body").animate({ scrollTop: 0 }, 600);
@@ -331,9 +325,7 @@ $(function	()	{
 });
 
 $(window).load(function() {
-	//Stop preloading animation
-	Pace.stop();
-	
+
 	// Fade out the overlay div
 	$('#overlay').fadeOut(800);
 	
